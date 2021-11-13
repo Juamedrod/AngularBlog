c74e7e4454994ba3a061a7162b3ab9a76118a177
JM_Medina <juamedrod@hotmail.es>
2021-11-13 13:01:42 +0100

    Form complete

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./formulario.component.css']
 })
 export class FormularioComponent implements OnInit {
+  formulario: FormGroup;
 
-  constructor() { }
+  constructor() {
+    this.formulario = new FormGroup({
+      titulo: new FormControl('', [Validators.required, Validators.minLength(5)]),
+      texto: new FormControl('', [Validators.required]),
+      autor: new FormControl('', [Validators.required]),
+      imagen: new FormControl(''),
+      fecha: new FormControl(new Date().toLocaleDateString('en-CA'), [Validators.required]),
+      categoria: new FormControl('', [Validators.required])
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    console.log(this.formulario.value);
+  }
+
+  checkError(controlName: string, error: string, touched: boolean): boolean | undefined {
+    return touched ? this.formulario.get(controlName)?.hasError(error) && this.formulario.get(controlName)?.touched : this.formulario.get(controlName)?.hasError(error);
   }
 
 }
